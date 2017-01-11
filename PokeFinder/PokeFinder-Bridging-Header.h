362e87fff0cd4cc7adf09a3f2adf76d01c15260e
Tony Merritt <Klavar@Meninx.home>
2017-01-11 21:32:46 +0000

    Pokemon Finder App
    This app is can be used to plot the places Pokemon have been found so
    other people can use the map to locate them with directions. The
    location is stored in Firebase Database online.

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Tony Merritt on 09/01/2017.
+//  Copyright © 2017 Tony Merritt. All rights reserved.
+//
+
+#import "GeoFire.h"
